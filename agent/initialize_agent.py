a586385f3199191403afeb63a9d78bc56e1c39e1
Alec Chen <93971719+0xAlec@users.noreply.github.com>
2024-11-08 12:51:48 -0500

    save wallet info

--- agent/initialize_agent.py
+++ agent/initialize_agent.py
@@ -9,27 +9,36 @@ from langgraph.prebuilt import create_react_agent
 from cdp_langchain.agent_toolkits import CdpToolkit
 from cdp_langchain.utils import CdpAgentkitWrapper
 
+from db.wallet import add_wallet_info, get_wallet_info
 
 def initialize_agent():
     """Initialize the agent with CDP Agentkit."""
     # Initialize LLM.
     llm = ChatOpenAI(model=constants.AGENT_MODEL)
 
-    # Read wallet data from environment variable
+    # Read wallet data from environment variable or database
     wallet_id = os.getenv(constants.WALLET_ID_ENV_VAR)
     wallet_seed = os.getenv(constants.WALLET_SEED_ENV_VAR)
+    wallet_info = json.loads(get_wallet_info()) if get_wallet_info() else None
 
     # Configure CDP Agentkit Langchain Extension.
     values = {}
-    if wallet_id and wallet_seed:
-        # If there is a wallet configuration in environment variables, use it
-        print("Initialized CDP Agentkit with wallet data:", wallet_id, wallet_seed, flush=True)
+
+    # Load agent wallet information from database or environment variables
+    if wallet_info:
+        wallet_id = wallet_info["wallet_id"]
+        wallet_seed = wallet_info["seed"]
+        print("Initialized CDP Agentkit with wallet data from database:", wallet_id, wallet_seed, flush=True)
+        values = {"cdp_wallet_data": json.dumps({ "wallet_id": wallet_id, "seed": wallet_seed })}
+    elif wallet_id and wallet_seed:
+        print("Initialized CDP Agentkit with wallet data from environment:", wallet_id, wallet_seed, flush=True)
         values = {"cdp_wallet_data": json.dumps({ "wallet_id": wallet_id, "seed": wallet_seed })}
 
     agentkit = CdpAgentkitWrapper(**values)
 
     # Export and store the updated wallet data back to environment variable
-    # wallet_data = agentkit.export_wallet()
+    wallet_data = agentkit.export_wallet()
+    add_wallet_info(json.dumps(wallet_data))
 
     # Initialize CDP Agentkit Toolkit and get tools.
     cdp_toolkit = CdpToolkit.from_cdp_agentkit_wrapper(agentkit)
